20307afee971a8774730bb4397267628267a50e5
sumanthvoruganti <vorugantisumanth93@gmail.com>
2023-11-27 19:29:49 +0530

    commit_27/11/2023

--- lodash.js
+++ lodash.js
@@ -1,3 +1,17 @@
-var _=require("lodash");
-let res=_.partition([2,3,6,7,4,9],x=>x%2);
-console.log(res);
+//even partition using lodash
+// var _=require("lodash");
+// let res=_.partition([2,3,6,7,4,9],x=>x%2);
+// console.log(res);
+
+
+var lodashObj = require('lodash');
+isEven = (n) => {
+    var x = n % 2;
+    return x;
+}
+
+var result = lodashObj.partition([1,2,3,4], isEven);
+console.log(result);
+
+var r1 = lodashObj.chunk(['a','b','c','d'], 3);
+console.log(r1);
